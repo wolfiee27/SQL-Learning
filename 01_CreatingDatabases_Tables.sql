ea86322457ddcaa91488b15c25b1fd8b27391a29
Gokul <36274337+wolfiee27@users.noreply.github.com>
2025-03-07 11:05:43 +0530

    Update section 1 contents

--- 01_CreatingDatabases_Tables.sql
+++ 01_CreatingDatabases_Tables.sql
@@ -6,3 +6,26 @@ use pratise;
 
 -- display the selected database -- 
 Select database();
+
+-- To drop a database -- 
+DROP DATABASE pratise;
+
+-- create a database --
+CREATE DATABASE pet_shop;
+
+--create table -- 
+CREATE TABLE cats (
+    name VARCHAR(50),
+    age INT
+);
+
+-- display all columns in tha table --
+SHOW COLUMNS FROM cats;
+-- or --
+DESC cats;
+
+--display all tables in the database --
+SHOW TABLES;
+
+-- DROP TABLE --
+DROP TABLE cats;
